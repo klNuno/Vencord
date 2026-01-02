bc986c364fbd0ca926422ba69c8020ef7506bb11
meetsu <nefreex@gmail.com>
2026-01-02 04:04:43 +0100

    first implementation of BackseatMode added "Disable on mute" volume slider added conditions for disabling the mode

---
+++ src/plugins/backseatMode/README.md
@@ -0,0 +1,3 @@
+# BackseatMode
+
+Lower voice chat output volume with middle-click on the deafen button
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    meetsu: {
+        name: "meetsu",
+        id: 741008885556707398n
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
